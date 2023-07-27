4ce1e1144e5a755b9356b846c235b65c37b65f60
Q Z <13253467705@163.com>
2023-07-27 10:49:04 +0800

    1、功能优化

--- Foundation+Extension/NotificationCenter+Extension.swift
+++ Foundation+Extension/NotificationCenter+Extension.swift
@@ -9,20 +9,20 @@ import Foundation
 
 extension NotificationCenter {
 
-    class func registerNotification(_ observer: Any, _ selector: Selector, name: NNNotificationName) {
+    class func registerNotification(_ observer: Any, _ selector: Selector, name: NotificationName) {
         self.default.addObserver(observer, selector: selector, name: NSNotification.Name(rawValue: name.rawValue), object: nil)
     }
 
-    class func postNotification(name: NNNotificationName, userInfo: [AnyHashable : Any]?) {
+    class func postNotification(name: NotificationName, userInfo: [AnyHashable : Any]?) {
         self.default.post(name: NSNotification.Name(rawValue: name.rawValue), object: nil, userInfo: userInfo)
     }
 
-    class func remove(_ observer: NNNotificationName) {
+    class func remove(_ observer: NotificationName) {
         self.default.removeObserver(observer)
     }
 
     //只适合常驻内存
-    class func observe(notificationName: NNNotificationName, block: @escaping (Notification) -> Void) {
+    class func observe(notificationName: NotificationName, block: @escaping (Notification) -> Void) {
         NotificationCenter.default.addObserver(forName: NSNotification.Name(rawValue: notificationName.rawValue), object: nil, queue: OperationQueue.main, using: block)
     }
 }
